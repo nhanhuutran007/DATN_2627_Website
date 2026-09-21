5150cddd347f5e5a1194d39a9ca0a1b99b7be99f
Trần Hữu Nhân <nhanhuutran007@gmail.com>
2026-09-22 01:17:24 +0700

    chore(repo): untrack legacy baocao files and ignore terraform state
    Remove baocao/ (legacy PHP project) and the nhlsports SQL dump from the index; files stay on disk. Ignore terraform state, .terraform/ and tfvars so secrets are never committed.

--- baocao/admin.php
+++
@@ -1,1 +0,0 @@
-<?php header("location: /baocao/admin");
--- baocao/view/php/xacThucAdmin.php
+++
@@ -1,7 +0,0 @@
-<?php
-$user = isset($_SESSION['info']) ? $_SESSION['info'] : [];
-if (empty($user)) {
-    $_SESSION['error_msg'] = "Đăng nhập để truy cập quyền admin!";
-    header("Location: /baocao/login");
-    exit;
-}
